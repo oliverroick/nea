fb36a53bb686395afc92750ea40aca6cf77ff09d
Oliver Roick <oliver.roick@gmail.com>
2018-02-18 13:15:29 +0100

    Use env variable for feeds file

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+import os
 from nea import download, parser, templates, mail
 
 if __name__ == "__main__":
     feed_sources = []
-    with open('feeds.txt') as feeds:
+    with open(os.environ.get('FEEDS'), 'feeds.txt') as feeds:
         feed_sources = download.get_feeds(feeds)
 
     new_blogs = (parser.parse_blog(feed) for feed in feed_sources)
