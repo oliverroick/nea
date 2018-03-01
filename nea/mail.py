d86bc0ae6012182d8cda8dc7ab171fdc28a8197c
Oliver Roick <oliver.roick@gmail.com>
2018-03-01 06:44:42 -0500

    Send HTM email

--- nea/mail.py
+++ nea/mail.py
@@ -1,19 +1,21 @@
 import os
 import smtplib
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 def send_mail(content):
     email_from = os.environ['DIGEST_MAIL']
     email_to = [os.environ['DIGEST_MAIL']]
 
-    msg = EmailMessage()
+    msg = MIMEMultipart()
     msg['From'] = email_from
     msg['To'] = ', '.join(email_to)
     msg['Subject'] = 'Weekly digest'
 
-    msg.set_content(content)
+    body = MIMEText(content, 'html')
+    msg.attach(body)
 
-    smtp = smtplib.SMTP('localhost')
+    smtp = smtplib.SMTP('localhost', 1025)
     smtp.sendmail(email_from, email_to, msg.as_string())
     smtp.close()
