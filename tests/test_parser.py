f1ce3b74b3d8823254962746b5bb734c85b54c13
Oliver Roick <oliver.roick@gmail.com>
2018-09-29 15:00:41 +0200

    Parse Atom Feeds

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,6 @@
 from xml.etree import ElementTree as etree
 from datetime import date, timedelta
-from lambdas.parser import util, rss
+from lambdas.parser import util, rss, atom
 
 
 # UTILS
@@ -16,7 +16,7 @@ def test_is_recent():
 
 
 # RSS
-def test_parse_item():
+def test_rss_parse_item():
     item_parameters = {
         'title': 'Some title',
         'link': 'http://example.com',
@@ -34,7 +34,7 @@ def test_parse_item():
     assert rss.parse_item(item) == item_parameters
 
 
-def test_parse():
+def test_rss_parse():
     recent_date = date.today() - timedelta(days=3)
     recent_date_string = recent_date.strftime(rss.pub_format)
 
@@ -73,3 +73,73 @@ def test_parse():
 
 
 # ATOM
+def test_atom_parse_item():
+    item_parameters = {
+        'title': 'Some title',
+        'link': 'http://example.com',
+        'date': date(2018, 9, 17)
+    }
+    item_string = """
+        <entry>
+            <title>{title}</title>
+            <link href="{link}" />
+            <published>2018-09-17T00:00:00+00:00</published>
+            <content>Lots of words</content>
+        </entry>
+    """
+    item = etree.fromstring(item_string.format(**item_parameters))
+    assert atom.parse_item(item) == item_parameters
+
+
+def test_atom_parse_item_with_negative_time_diff():
+    item_parameters = {
+        'title': 'Some title',
+        'link': 'http://example.com',
+        'date': date(2018, 9, 17)
+    }
+    item_string = """
+        <entry>
+            <title>{title}</title>
+            <link href="{link}" />
+            <published>2018-09-17T00:00:00-02:00</published>
+            <content>Lots of words</content>
+        </entry>
+    """
+    item = etree.fromstring(item_string.format(**item_parameters))
+    assert atom.parse_item(item) == item_parameters
+
+
+def test_atom_parse():
+    recent_date = date.today() - timedelta(days=3)
+    recent_date_string = recent_date.strftime(atom.pub_format)
+
+    older_date = date.today() - timedelta(days=17)
+    older_date_string = older_date.strftime(atom.pub_format)
+
+    item_string = """
+        <feed>
+            <title>Blog Title</title>
+            <entry>
+                <title>Title</title>
+                <link href="http://example.com" />
+                <published>{recent_date}+00:00</published>
+                <content>Lots of words</content>
+            </entry>
+            <entry>
+                <title>Title</title>
+                <link href="http://example.com" />
+                <published>{recent_date}+00:00</published>
+                <content>Lots of words</content>
+            </entry>
+            <entry>
+                <title>Title</title>
+                <link href="http://example.com" />
+                <published>{older_date}+00:00</published>
+                <content>Lots of words</content>
+            </entry>
+        </feed>
+    """.format(recent_date=recent_date_string, older_date=older_date_string)
+
+    parsed = atom.parse(etree.fromstring(item_string))
+    assert parsed['title'] == 'Blog Title'
+    assert len(list(parsed['items'])) == 2
